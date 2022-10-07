38aa3212190d78780513e88012698171aed43e71
catherinecunningham <cfunk512@gmail.com>
2022-10-06 20:07:10 -0700

    set up random work generator api

--- assets/js/index.js
+++ assets/js/index.js
@@ -7,3 +7,14 @@
 // completed items allows user to write in notes of completed work
 
 // resources section filled with helpful links or useful search APIs
+
+// APIs for Curiosity Corner
+//random word generator
+function randomWord() {
+    
+    fetch('https://xkubist-random-word-v1.p.rapidapi.com/run.cgi')
+        .then(response => response.json())
+        .then(response => console.log(data))
+}
+
+
