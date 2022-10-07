1f13254ac0600b5145aae6d43caa1694f379df56
catherinecunningham <cfunk512@gmail.com>
2022-10-06 20:52:46 -0700

    work on server boiler plate

--- server.js
+++ server.js
@@ -1,6 +1,14 @@
 const express = require('express')
 const app = express()
+const path = require('path')
+const fs = require('fs')
 
-app.listen(3001, () => {
-    console.log('something here')
-})
+const PORT = process.env.PORT || 3001
+
+app.use(express.json())
+app.use(express.urlencoded({
+    extended: true
+}))
+app.use(express.static('assets'))
+
+app.listen(PORT, () => console.log(`listening on PORT: ${PORT}`))
