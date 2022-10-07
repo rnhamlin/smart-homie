c58f085a333c15cc4b0221d4255cf25567d8dbb0
catherinecunningham <cfunk512@gmail.com>
2022-10-06 21:03:53 -0700

    add notation to server file

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require('express')
 const app = express()
 const path = require('path')
 const fs = require('fs')
+const { notStrictEqual } = require('assert')
 
 const PORT = process.env.PORT || 3001
 
@@ -10,5 +11,16 @@ app.use(express.urlencoded({
     extended: true
 }))
 app.use(express.static('assets'))
+
+//add get routes
+
+//get route for home directory 
+//app.get('/', (req, res) => {
+//res.sendFile(path.join(_dirname, 'insert directory name here to get inputted data'))
+//})
+
+// repeat above for multiple get routes
+
+//add post routes
 
 app.listen(PORT, () => console.log(`listening on PORT: ${PORT}`))
