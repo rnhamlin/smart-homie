b425f5bfb17e72112dd489af47aab4ffcb671645
alexuschristensen <alexusdc6@gmail.com>
2022-10-06 21:00:41 -0700

    added to hooks and routes

--- assets/js/user.js
+++ assets/js/user.js
@@ -39,8 +39,17 @@ USER.init(
     },
     {
        hooks: {
+            async beforeCreate(newUserData) {
+                newUserData.password = await bcrypt.hash(newUserData.password, 10);
+                return newUserData;
+            },
 
-       },
+            async beforeUpdate(updatedUserData) {
+                updatedUserData.password = await bcrypt.hash(updatedUserData.password, 10);
+                return updatedUserData
+            }
+        },
+
        sequelize, 
        timestamps: false, 
        freezeTableName: true, 
