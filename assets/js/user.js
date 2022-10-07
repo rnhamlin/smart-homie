193f019efca80196e053520aed85e321478d2411
alexuschristensen <alexusdc6@gmail.com>
2022-10-06 20:39:45 -0700

    added hashing and hooks

---
+++ assets/js/user.js
@@ -0,0 +1,50 @@
+const bcrypt = require('bcrypt');
+const { DataTypes } = require('sequelize');
+const { UPSERT } = require('sequelize/types/query-types');
+
+bcrypt.hash(myPlaintestPassword, saltRounds).then(function(hash) {
+    
+});
+
+USER.init(
+    {
+        id: {
+            type: DataTypes.INTEGER, 
+            allowNull: false, 
+            primaryKey: true, 
+            autoIncre,ent: true 
+        },
+        
+        username: {
+            type: DataTypes.STRING, 
+            allowNull: false
+        },
+
+        email: {
+            type: DataTypes.STRING, 
+            allowNull: false, 
+            unique: true,
+            validate: {
+                isEmail: true
+            }
+        },
+
+        password: {
+            type: DataTypes.STRING, 
+            allowNull: false, 
+            validate: {
+                len: [4]
+            }
+        }
+    },
+    {
+       hooks: {
+
+       },
+       sequelize, 
+       timestamps: false, 
+       freezeTableName: true, 
+       underscored: true, 
+       modelName: 'user' 
+    }
+);
