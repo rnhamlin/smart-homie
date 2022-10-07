4f85a1f033a0bd7b1060685c75f32e706589210d
catherinecunningham <cfunk512@gmail.com>
2022-10-06 20:44:56 -0700

    update second api for NASA

--- assets/js/index.js
+++ assets/js/index.js
@@ -13,8 +13,17 @@
 function randomWord() {
     
     fetch('https://xkubist-random-word-v1.p.rapidapi.com/run.cgi')
-        .then(response => response.json())
-        .then(response => console.log(data))
+        .then((res) => res.json())
+        .then((data) => {
+            console.log(data)
+        })
 }
 
+function marsWeather() {
 
+    fetch('https://api.nasa.gov/insight_weather/?api_key=faCfjgrFrO81fgt3XAlsJyNqFWKWll17NRAUNcDA&feedtype=json&ver=1.0')
+    .then((res) => res.json())
+    .then((data) => {
+        console.log(data)
+    })
+}
