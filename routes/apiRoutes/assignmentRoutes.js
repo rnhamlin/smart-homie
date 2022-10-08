ac7245d14b39809a70de8cb2019c16f78717ebf3
Abigail Queen <107533064+Abi-Queen@users.noreply.github.com>
2022-10-08 05:10:57 -0700

    drafted assignment routes outline

--- routes/apiRoutes/assignmentRoutes.js
+++ routes/apiRoutes/assignmentRoutes.js
@@ -3,12 +3,57 @@ const router = express.Router();
 const db = require('../../db/connection');
 
 // Get all assignments
-
+router.get('/assignments', (req, res) => {
+    const sql = `SELECT * FROM assignments`;
+  
+    db.query(sql, (err, rows) => {
+      if (err) {
+        res.status(500).json({ error: err.message });
+        return;
+      }
+      res.json({
+        message: 'success',
+        data: rows
+      });
+    });
+  });
 
 // Get single assignment
-
+router.get('/assignments/:id', (req, res) => {
+    const sql = `SELECT * FROM assignments WHERE id = ?`;
+    const params = [req.params.id];
+  
+    db.query(sql, params, (err, row) => {
+      if (err) {
+        res.status(400).json({ error: err.message });
+        return;
+      }
+      res.json({
+        message: 'success',
+        data: row
+      });
+    });
+  });
 
 // Delete an assignment
-
+router.delete('/party/:id', (req, res) => {
+    const sql = `DELETE FROM assignments WHERE id = ?`;
+  
+    db.query(sql, req.params.id, (err, result) => {
+      if (err) {
+        res.status(400).json({ error: res.message });
+      } else if (!result.affectedRows) {
+        res.json({
+          message: 'Assignment not found'
+        });
+      } else {
+        res.json({
+          message: 'deleted',
+          changes: result.affectedRows,
+          id: req.params.id
+        });
+      }
+    });
+  });
 
 module.exports = router;
