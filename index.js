846d59388cf813527f93233df23d4340d6cc9e56
catherinecunningham <cfunk512@gmail.com>
2022-10-10 20:27:31 -0700

    combine index.js files, set up form variables

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
+// Variables
+var goal = document.querySelector("#goal")
+var assignment = document.querySelector("#assignment-name")
+var subject = document.querySelector("#subject")
+var grade = document.querySelector("#grade")
+var standard = document.querySelector("#standard")
 
+
+
+
+//APIs for Curiosity Corner
+function randomWord() {
+    
+    fetch('https://xkubist-random-word-v1.p.rapidapi.com/run.cgi')
+        .then((res) => res.json())
+        .then((data) => {
+            console.log(data)
+        })
+}
+
+function marsWeather() {
+
+    fetch('https://api.nasa.gov/insight_weather/?api_key=faCfjgrFrO81fgt3XAlsJyNqFWKWll17NRAUNcDA&feedtype=json&ver=1.0')
+    .then((res) => res.json())
+    .then((data) => {
+        console.log(data)
+    })
+}
