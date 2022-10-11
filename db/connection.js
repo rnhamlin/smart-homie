49fea575053276b897c3f77985245691222d0e0f
Abigail Queen <107533064+Abi-Queen@users.noreply.github.com>
2022-10-11 05:25:56 -0700

    updating routes for sequelize

--- db/connection.js
+++ db/connection.js
@@ -3,12 +3,13 @@ const Sequelize = require('sequelize');
 
 require('dotenv').config();
 
-const sequelize = new Sequelize('smart-homie_db', 'username', 'password', {
+const sequelize = new Sequelize(process.env_DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
   host: 'localhost',
   user: 'root',
   port: 3306
 });
 
+// do I comment this createConnection out/delete since we have sequelize now above?
 const db = mysql.createConnection({
   host: 'localhost',
   user: 'root',
--- routes/apiRoutes/index.js
+++ routes/apiRoutes/index.js
@@ -1,8 +1,11 @@
 const express = require('express');
 const router = express.Router();
+const userRoutes = require('./user-routes.js');
+const assignmentRoutes = require('./assignment-routes.js');
+const studentRoutes = require('./studentRoutes');
 
-router.use(require('./assignmentRoutes'));
-router.use(require('./studentRoutes'))
-//add other routes
+router.use('/users', userRoutes);
+router.use('/assignments', assignmentRoutes); 
+router.use('/students', studentRoutes);
 
 module.exports = router;
