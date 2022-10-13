5198624350c83627fd454956f328eef40ec71c3a
catherinecunningham <cfunk512@gmail.com>
2022-10-12 20:22:12 -0700

    more work on random word api

--- index.js
+++ index.js
@@ -5,8 +5,9 @@ var subject = document.querySelector("#subject")
 var grade = document.querySelector("#grade")
 var standard = document.querySelector("#standard")
 
-var wordBtn = document.querySelector("random-word-button")
-var marsBtn = document.querySelector("mars-weather-button")
+var wordBtn = document.getElementById("random-word-button")
+var randomWord = document.getElementById("random-word")
+var marsBtn = document.getElementById("mars-weather-button")
 
 
 //Capture input for assignments to fill in "You're Working On"
@@ -20,13 +21,24 @@ var marsBtn = document.querySelector("mars-weather-button")
 //finish APIs for the CC
 //APIs for Curiosity Corner
 
-//wordBtn.addEventListener('click', function () {
-  //  fetch("https://xkubist-random-word-v1.p.rapidapi.com/run.cgi")
-    //    .then((res) => res.json())
-      //  .then((data) => {
-        //    console.log(data)
-        //})
-//})
+wordBtn.addEventListener('click', function () {
+    const options = {
+        method: 'GET',
+        headers: {
+            'X-RapidAPI-Key': '3561faa0a4msh79219792fe4522ap13fbb8jsn0b78e7370034',
+            'X-RapidAPI-Host': 'random-words5.p.rapidapi.com'
+        }
+    };
+
+    fetch('https://random-words5.p.rapidapi.com/getMultipleRandom?count=5', options)
+        .then((res) => res.json())
+        .then((data) => {
+            console.log(data)
+            randomWord.textContent = data[0].randomWord.value
+        })
+
+
+})
 
 
 
