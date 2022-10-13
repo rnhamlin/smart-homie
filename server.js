9e75de41a4cac9b8b278086806ff219a1b96087c
G Codes <graciela.mejia1@yahoo.com>
2022-10-12 19:47:13 -0700

    heroku update code

--- server.js
+++ server.js
@@ -1,8 +1,10 @@
-const express = require('express')
-const db = require ('./db/connection');
-const apiRoutes = require('./routes/apiRoutes');
+const express = require("express");
+const PORT = process.env.PORT || 3001;
+const app = express();
+const db = require("./db/connection");
+const apiRoutes = require("./routes/apiRoutes");
 
-//get route for home directory 
+//get route for home directory
 //app.get('/', (req, res) => {
 //res.sendFile(path.join(_dirname, 'insert directory name here to get inputted data'))
 //})
@@ -11,29 +13,26 @@ const apiRoutes = require('./routes/apiRoutes');
 
 //add post routes
 
-const PORT = process.env.PORT || 3001;
-const app = express();
-
 // Express middleware
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
 // Use apiRoutes
-app.use('/api', apiRoutes);
+app.use("/api", apiRoutes);
 
 // Default response for any other request (Not Found)
 app.use((req, res) => {
-    res.json({
-        message: 'Working'
-    });
+  res.json({
+    message: "Working",
+  });
   res.status(404).end();
 });
 
 // Start server after DB connection
-db.connect(err => {
+db.connect((err) => {
   if (err) throw err;
-  console.log('Database connected.');
+  console.log("Database connected.");
   app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`);
+    console.log(`Server running on port ${PORT}!`);
   });
 });
