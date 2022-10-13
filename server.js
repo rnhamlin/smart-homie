e1b9cc4f5a2c902908ae7f8656bedea755c9ac7a
alexuschristensen <alexusdc6@gmail.com>
2022-10-12 20:23:13 -0700

    login page

--- server.js
+++ server.js
@@ -7,6 +7,23 @@ const apiRoutes = require('./routes/apiRoutes');
 //res.sendFile(path.join(_dirname, 'insert directory name here to get inputted data'))
 //})
 
+//connects session to the sequelize database 
+const session = require('express-session');
+
+const SequelizeStore = require('connect-session-sequelize')(session.Store);
+
+const sess = {
+  secret: 'Super secret secret',
+  cookie: {},
+  resave: false,
+  saveUninitialized: true,
+  store: new SequelizeStore({
+    db: sequelize
+  })
+};
+
+app.use(session(sess));
+
 // repeat above for multiple get routes
 
 //add post routes
