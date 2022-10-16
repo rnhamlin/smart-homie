aa536a5c1ae5ed7f2639226c815d6286d61157cd
G Codes <graciela.mejia1@yahoo.com>
2022-10-15 19:42:56 -0700

    "fix typo"

--- config/connection.js
+++ config/connection.js
@@ -1,12 +1,17 @@
 //import the Sequelize constructor from the library
-const Sequelize = require('sequelize');
-require('dotenv').config();
+const Sequelize = require("sequelize");
+require("dotenv").config();
 
 //create connection to database, pass in MySQL login ingo
-const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
-    host: 'localhost',
-    dialect: 'mysql',
-    port: 3306
-});
+const sequelize = new Sequelize(
+  process.env.DB_NAME,
+  process.env.DB_USER,
+  process.env.DB_PASSWORD,
+  {
+    host: "localhost",
+    dialect: "mysql",
+    port: 3306,
+  }
+);
 
-module.exports = sequelize; 
+module.exports = sequelize;
