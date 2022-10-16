7f0fdfdf62b046fb55adf7c2e946e5ba5234aa38
Abigail Queen <107533064+Abi-Queen@users.noreply.github.com>
2022-10-16 06:24:41 -0700

    updated router and express const in apiRoutes/index

--- routes/apiRoutes/index.js
+++ routes/apiRoutes/index.js
@@ -1,5 +1,5 @@
-const express = require("express");
-const router = express.Router();
+// const express = require("express");
+const router = require('express').Router();
 
 const userRoutes = require("./user-routes.js");
 const postRoutes = require("./post-routes");
@@ -12,3 +12,20 @@ router.use("./assignmentRoutes", assignmentRoutes);
 router.use("./studentRoutes", studentRoutes);
 
 module.exports = router;
+
+
+// const router = require('express').Router();
+
+// const apiRoutes = require('/apiRoutes/index.js');
+// const userRoutes = require("./user-routes.js");
+// const postRoutes = require("./post-routes");
+// const assignmentRoutes = require("./assignmentRoutes.js");
+// const studentRoutes = require("./studentRoutes");
+
+// router.use("./user-routes", userRoutes);
+// router.use("./post-routes", postRoutes);
+// router.use("./assignmentRoutes", assignmentRoutes);
+// router.use("./studentRoutes", studentRoutes);
+// router.use('/apiRoutes', apiRoutes); 
+
+// module.exports = router;
